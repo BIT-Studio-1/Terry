3d9ad4f54e5256d764e0dea5fc85dc3e36a2dc76
LewisJHill <hilllj4@student.op.ac.nz>
2021-05-20 15:53:07 +1200

    Implemented basic movement between rooms

--- Terry demo/Program.cs
+++ Terry demo/Program.cs
@@ -1,184 +1,82 @@
 ﻿using System;
 
-namespace Terry
+namespace Command_Line_Adventure
 {
     class Program
     {
-        public static void Hallway_cells()
+        public static void Dungeon()
         {
-            Console.WriteLine("You are in a long and dark hallway, you can hear someone snoring close to you . ");
-            Console.WriteLine("Would you like to check or not?");
-        }
-            static void Main(string[] args)
-                
-            {
-
-            string desition;
-            int stealth = 1, strength = 1, cunning = 1;
-            int result;
-            bool victory = false;
-            string item1 = "", item2 = "", item3 = "";
-            Random rand = new Random();
+            string temp;
+            int input;
 
             do
             {
+
+
+                Console.Clear();
+                Console.WriteLine("You are in the Dungeon");
                 Console.WriteLine();
-                Console.WriteLine("You wake up in your cell with a noise, it did sound like someone unlocked the gate.");
-                
-                do{
-                    Console.WriteLine("1) Go and Investigate");
-                    Console.WriteLine("2) Open");
-                    Console.WriteLine("3) Stay");
-                    Console.WriteLine("\n");
-                    Console.WriteLine("CHOOSE AN OPTION: ");
-                    desition = Console.ReadLine();
-                    Console.Clear();
+                Console.WriteLine("Your options are...");
+                Console.WriteLine("- [1] Go to Hallway");
+                Console.WriteLine();
 
-                    switch (desition)
+                temp = Console.ReadLine();
+                input = Convert.ToInt32(temp);
+                Console.Clear();
 
-                    {
-                        case "1":
-                        case "2":
-
-                                Hallway_cells();
-                            break;
-                        case "3":
-                            Console.WriteLine("You are afraid, but this may be your only chance to get out of here. ");
-                            break;
-                        default:
-                            Console.WriteLine("Not a valid action ");
-                            break;
-
-
-
-                    }
-                    Console.WriteLine();
-                } while ((desition != "open") || (desition != "Go and Investigate"));
-                desition = Console.ReadLine();
-                switch (desition)
+                switch (input)
                 {
-                    case "yes":
-                    case "y":
-                        Console.WriteLine("The sleeping person seems to be a guard, there is a key hanging from his belt . ");
-                        Console.WriteLine("Try to get the key? ");
-                        if (stealth == 1)
-                        {
-                            Console.WriteLine($"Your stealth level is {stealth}. You need 3 or more to win ");
-                        }
-                        else if (stealth == 2)
-                        {
-                            Console.WriteLine($"Your stealth level is {stealth}. You need a 2 or more to win ");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"Your stealth level is {stealth}. You need a 2 or more to win ");
-                        }
-
-
-                        Console.WriteLine("");
-                        desition = Console.ReadLine();
-                        if (desition == "yes")
-                        {
-                            switch (stealth)
-                            {
-                                case 1:
-                                    Console.WriteLine("Stealth required = 3 ");
-                                    result = rand.Next(1, 7);
-                                    if (result >= 2)
-                                    {
-                                        Console.WriteLine($"Result: {result}. You stole the key. ");
-                                        item1 = "key";
-                                        victory = true;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"Result: {result}. The guard woke up!!! You get caught and sent back to your cell.");
-
-                                    }
-                                    break;
-                                case 2:
-                                    Console.WriteLine("Stealth required = 2 ");
-                                    result = rand.Next(1, 7);
-                                    if (result >= 2)
-                                    {
-                                        Console.WriteLine($"Result: {result}. You stole the key. ");
-                                        item1 = "key";
-                                        victory = true;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"Result: {result}. The guard woke up!!! You get caught and sent back to your cell.");
-
-                                    }
-                                    break;
-                                case 3:
-                                    Console.WriteLine("Stealth required = 1 ");
-                                    result = rand.Next(1, 7);
-                                    if (result >= 1)
-                                    {
-                                        Console.WriteLine($"Result: {result}. You stole the key. ");
-                                        item1 = "key";
-                                        victory = true;
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"Result: {result}. The guard woke up!!! You get caught and sent back to your cell.");
-
-                                    }
-                                    break;
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("The risk is not worth it. You keep walking. ");
-                            victory = true;
-                        }
+                    case 1:
+                        Hallway();
                         break;
-
-                    case "no":
-                    case "n":
-                        Console.WriteLine("The risk is not worth it. You keep walking. ");
-                        victory = true;
-
-                        break;
-
-
                     default:
-                        Console.WriteLine("Not a valid action ");
+                        Console.WriteLine();
+                        Console.WriteLine("You have entered an invalid input");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadLine();
                         break;
-
                 }
-            } while (victory != true);
-            Console.WriteLine("You reach an end point. There is a door in front of you, but you need a key.");
-            if ((item1 == "key" || item2 == "key" || item3 == "key"))
+            } while (input != 0);
+        }
+        public static void Hallway()
+        {
+            string temp;
+            int input;
+
+            Console.Clear();
+            Console.WriteLine("You are in the Hallway");
+            Console.WriteLine();
+            Console.WriteLine("Your options are...");
+            Console.WriteLine("- [1] Go into the Dungeon");
+            Console.WriteLine("- [2] Go Left ");
+            Console.WriteLine("- [3] Go Right ");
+            Console.WriteLine();
+            temp = Console.ReadLine();
+            input = Convert.ToInt32(temp);
+            Console.Clear();
+
+            switch (input)
             {
-                do
-                {
-                    Console.WriteLine("Do you want to try the key? ");
-                    desition = Console.ReadLine();
-                    switch (desition)
-                    {
-                        case "yes":
-                        case "y":
-                            Console.WriteLine("The key fits the hole and you get into a new room.");
-                            break;
-                        case "no":
-                        case "n":
-                            Console.WriteLine("It's nowhere else to go.");
-                            break;
+                case 1:
+                    Dungeon();
+                    break;
+                case 2:
 
-                    }
-                } while (desition != "yes" && desition != "y");
+                    break;
+                case 3:
+
+                    break;
+                default:
+                    Console.WriteLine();
+                    Console.WriteLine("You have entered an invalid input");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                    break;
             }
-            else
-            {
-                Console.WriteLine("You dont have a key.");
-            }
-
-
-
-
-
+        }
+        public static void Main()
+        {
+            Dungeon();
             Console.ReadLine();
         }
     }
