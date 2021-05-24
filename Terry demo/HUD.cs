438c720a8564b8098d6795c2d8fe5b0680fe21dd
farhayden <haydefa1@student.op.ac.nz>
2021-05-24 12:34:32 +1200

    added a new class called HUD for a heads up display

---
+++ Terry demo/HUD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terry_demo
+{
+    class HUD
+    {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
