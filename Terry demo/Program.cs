34c4ef283941b328fa15dbcd515dd86d1c9ebf25
Mason Caldwell (1000076410) <CALDMH1@student.op.ac.nz>
2021-05-25 10:57:54 +1200

    added intro text to dungeon and fate

--- Terry demo/Program.cs
+++ Terry demo/Program.cs
@@ -165,10 +165,10 @@ namespace Command_Line_Adventure
 
 
                 Console.Clear();
-                Console.WriteLine("You are in the Dungeon");
+                Console.WriteLine("You wake up in a dark and dilapidated Dungeon, With no idea how you got here. \nHowever you see a faint light in the distance and notice that the door is slightly open.  ");
                 Console.WriteLine();
                 Console.WriteLine("Your options are...");
-                Console.WriteLine("- [1] Go to the Hallway");
+                Console.WriteLine("- [1] Go Investigate the Hallway");
                 Console.WriteLine("- [2] Stay Imprisoned");
                 Console.WriteLine("- [3] Check Inventory");
                 Console.WriteLine();
@@ -183,7 +183,7 @@ namespace Command_Line_Adventure
                         NorthMiddleHallway();
                         break;
                     case 2:
-                        Console.WriteLine("You die later that night");
+                        Console.WriteLine("You chose to stay. And with that your only chance of escape is gone, The Guards walk into the dungeon \nYou die later that night");
                         Console.ReadLine();
                         break;
                     case 3:
@@ -854,7 +854,7 @@ namespace Command_Line_Adventure
                 Inventory();
             }
 
-            Console.WriteLine();
+            Console.WriteLine("test intro text");
             Dungeon();
 
             Console.ReadLine();
