c443948d8d3ed2c2c506921c4e16681cfc77e15c
spooferjahks <caldmh1@student.op.ac.nz>
2021-06-09 13:46:25 +1200

    added new ending placeholder

--- Terry demo/Program.cs
+++ Terry demo/Program.cs
@@ -867,7 +867,10 @@ namespace Command_Line_Adventure
                     RightHallway();
                     break;
                 case 2:
-                    Secondfloor_greathall();
+                    //placeholder
+                    Console.WriteLine("You have escaped the Dungeon, But what do you do now?");
+                    Console.WriteLine("Get Revenge or Run like hell and hide for the rest of your life");
+                    Console.WriteLine("Maybe revenge isn't the best thing to do....yet");
                     break;
                 case 3:
                     Inventory();
