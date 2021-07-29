6609b19573d433f5a40702eda199bfa2f54b960a
farhayden <haydefa1@student.op.ac.nz>
2021-07-29 16:19:55 +1200

    added more methods to the Maps.cs class to hold the relevent room's ascii art maps

--- Terry demo/Maps.cs
+++ Terry demo/Maps.cs
@@ -6,6 +6,167 @@ namespace Terry_demo
 {
     class Maps
     {
-        private string map;
+        private string location;
+
+        public Maps(string location)
+        {
+            this.location = location;
+        }
+
+        public void OutsideDungeon()
+        {
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+        }
+        public void OutsideLairOfTheBeast()
+        {
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+        }
+        public void OutsideCrematorium()
+        {
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+        }
+        public void OutsideEmptyArmoury()
+        {
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+        }
+        public void OutsideGuardsChamber()
+        {
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"                                                                     ");
+        }
     }
 }
