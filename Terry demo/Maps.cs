151da603538d84860d4342a12a06346dcc076340
farhayden <haydefa1@student.op.ac.nz>
2021-08-02 13:47:19 +1200

    Nearly finished the first map for when the player exits the empty armoury

--- Terry demo/Maps.cs
+++ Terry demo/Maps.cs
@@ -108,34 +108,40 @@ namespace Terry_demo
         }
         public void OutsideEmptyArmoury()
         {
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
-            Console.WriteLine(@"                                                                     ");
+            Console.WriteLine(@"||==============================================================================||");
+            Console.WriteLine(@"||                                   |   ╔                 ╗                    ||");
+            Console.WriteLine(@"||                                   |    Lair of the beast                     ||");
+            Console.WriteLine(@"||                                   |   ╚                 ╝                    ||");
+            Console.WriteLine(@"||                                   |                                          ||");
+            Console.WriteLine(@"||                                   |                                          ||");
+            Console.WriteLine(@"||___________________________________|__________________________________________||");
+            Console.WriteLine(@"||               /|             /  /            /  /         /|                 ||");
+            Console.WriteLine(@"||______________/ |____________/^_/____________/^_/________ / |   ╔      ╗      ||");
+            Console.WriteLine(@"||╔           ╗|  | ╔       ╗                              |  |    Locked       ||");
+            Console.WriteLine(@"|| Crematorium |  |  Hallway                               |  |    Mystery      ||");
+            Console.WriteLine(@"||╚           ╝|  | ╚       ╝______________________        |  |     Room        ||");
+            Console.WriteLine(@"||             |  |        / |           /  /     /|       |  |   ╚      ╝      ||");
+            Console.WriteLine(@"||             | /|       /  |__________/^_/_____/ |       |  |                 ||");
+            Console.WriteLine(@"||             |/ |       |  |                   | |       | /|                 ||");
+            Console.WriteLine(@"||             | /|       |  |                   | |       |/ |                 ||");
+            Console.WriteLine(@"||             |/ |       |  |                   | |       | /|                 ||");
+            Console.WriteLine(@"||_____________|__|       |  |      ╔       ╗    | |       |/ |                 ||");
+            Console.WriteLine(@"||               /|       |  |       Dungeon     | |       |  |                 ||");
+            Console.WriteLine(@"||_____________ / |       |  |      ╚       ╝    | |       |  |                 ||");
+            Console.WriteLine(@"||             |  |       |  |                   | |       |  |                 ||");
+            Console.WriteLine(@"||             |  |       |  |                   | |       |  |                 ||");
+            Console.WriteLine(@"||             |  |       |  |                   | |       |  |                 ||");
+            Console.WriteLine(@"||             |  |       |  |___________________|_|       |  |                 ||");
+            Console.WriteLine(@"||             |  |       | /                     /        |  |                 ||");
+            Console.WriteLine(@"||             |  |       |/_____________________/         |  |                 ||");
+            Console.WriteLine(@"||             |  |                                        |  |                 ||");
+            Console.WriteLine(@"||             |  |                                        |  |                 ||");
+            Console.WriteLine(@"||             |  |                                        |  |                 ||");
+            Console.WriteLine(@"||             |  |____________________________________    |  |                 ||");
+            Console.WriteLine(@"||             |  |                                        |  |                 ||");
+            Console.WriteLine(@"||             |  |                                        |  |                 ||");
+            Console.WriteLine(@"||             |  |                                        |  |                 ||");
+            Console.WriteLine(@"||             |  |                                        |  |                 ||");
         }
         public void OutsideGuardsChamber()
         {
