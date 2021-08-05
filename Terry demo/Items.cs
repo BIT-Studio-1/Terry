0f7cabe22e124b46a94793dd72f52b95b4ff65e7
farhayden <haydefa1@student.op.ac.nz>
2021-08-05 16:42:12 +1200

    Set up a new class to store the item animations for when the player picks up one of the items specified in the class.

---
+++ Terry demo/Items.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Terry_demo
+{
+    class Items
+    {
+
+        public static void FathersRing()
+        {
+
+        }
+        public static void RustySword()
+        {
+
+        }
+        public static void KingsSword()
+        {
+
+        }
+        public static void GuardsArmour()
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
